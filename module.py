c2ec1a4de44a6d3b346cc52f3e1e00b7058619b4
AK Maurya <ankitkrmaurya82@gmail.com>
2026-06-24 11:46:38 +0530

    student marks predict

--- module.py
+++ module.py
@@ -1,4 +1,5 @@
 import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LinearRegression, LogisticRegression
