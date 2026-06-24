faafbaa6b730ae85d35299ab91cec19613cc1b0d
AK Maurya <ankitkrmaurya82@gmail.com>
2026-06-24 14:49:46 +0530

    COVID 19

--- module.py
+++ module.py
@@ -3,3 +3,4 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
