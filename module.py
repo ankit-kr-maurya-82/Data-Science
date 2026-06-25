d682095fc40ad229cfd33de8bbb5a9fff1149e04
AK Maurya <ankitkrmaurya82@gmail.com>
2026-06-25 14:52:03 +0530

    confusion_matrix

--- module.py
+++ module.py
@@ -1,7 +1,12 @@
 import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
